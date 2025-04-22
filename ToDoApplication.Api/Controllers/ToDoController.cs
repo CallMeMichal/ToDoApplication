2241d82c186e2f8e6708ec76196084f6a08a6450
Michal Tulej <michaltulej1@gmail.com>
2025-04-22 13:04:45 +0200

    basic controller structure, preview documentation

---
+++ ToDoApplication.Api/Controllers/ToDoController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ToDoApplication.Api.Controllers
+{
+    /// <summary>
+    /// Controller for managing todos
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ToDoController : Controller
+    {
+        /// <summary>
+        /// Get all todos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("")]
+        public async Task GetAllTodos()
+        {
+        }
+
+        /// <summary>
+        /// Get todo by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task GetTodoById(int id)
+        {
+        }
+
+        /// <summary>
+        /// Get incoming todos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("incoming")]
+        public async Task GetIncomingTodos()
+        {
+        }
+
+        /// <summary>
+        /// Create todo
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        public async Task CreateTodo()
+        {
+        }
+
+        /// <summary>
+        /// Update todo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task UpdateTodo(int id)
+        {
+        }
+
+        [HttpPatch("{id}/percent")]
+        public async Task SetTodoPercent(int id /*[FromBody] PercentUpdateModel percentModel*/)
+        {
+        }
+
+        /// <summary>
+        /// Delete todo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task DeleteTodo(int id)
+        {
+        }
+
+        /// <summary>
+        /// Mark todo as complete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("{id}/done")]
+        public async Task MarkDoneTodo(int id)
+        {
+        }
+    }
+}
