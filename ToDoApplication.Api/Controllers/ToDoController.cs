ffcc132689c3ec291f5ea9f52858bbc0d6b6948f
Michal Tulej <michaltulej1@gmail.com>
2025-04-23 09:41:07 +0200

    controller update

--- ToDoApplication.Api/Controllers/ToDoController.cs
+++ ToDoApplication.Api/Controllers/ToDoController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApplication.Api.Helpers;
+using ToDoApplication.Api.Validators;
 using ToDoApplication.Common.Interfaces;
 using ToDoApplication.Common.Models.Domain.Request;
 using ToDoApplication.Common.Models.Domain.Response;
@@ -49,10 +53,17 @@ namespace ToDoApplication.Api.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet("incoming")]
-        public async Task<List<GetIncomingTodosResponse>> GetIncomingTodos(DateTime dateTime)
+        public async Task<ActionResult<List<GetIncomingTodosResponse>>> GetIncomingTodos(DateTime dateTime)
         {
+            var result = ValidatorHelper.ValidateRequest(dateTime, new GetIncomingTodosValidator());
+
+            if (result != null)
+            {
+                return BadRequest(result);
+            }
+
             var response = await _IToDoService.GetIncomingTodos();
-            return response;
+            return Ok(response);
         }
 
 
@@ -65,6 +76,13 @@ namespace ToDoApplication.Api.Controllers
         [Route("")]
         public async Task<ApiResponse> CreateTodo(CreateTodoRequest request)
         {
+            var result = ValidatorHelper.ValidateRequest(request, new CreateTodoValidator());
+
+            if (result != null)
+            {
+                return result;
+            }
+
             var response = await _IToDoService.CreateTodo(request);
             return response;
         }
@@ -77,19 +95,32 @@ namespace ToDoApplication.Api.Controllers
         [HttpPut("{id}")]
         public async Task<ApiResponse> UpdateTodo(UpdateTodoRequest request)
         {
+            var result = ValidatorHelper.ValidateRequest(request, new UpdateTodoValidator());
+
+            if (result != null)
+            {
+                return result;
+            }
+
             var response = await _IToDoService.UpdateTodo(request);
             return response;
         }
 
         /// <summary>
-        /// Change Complete Percent of Todo
+        /// Set todo percent
         /// </summary>
         /// <param name="id"></param>
+        /// <param name="amount"></param>
         /// <returns></returns>
         [HttpPatch("{id}/percent")]
-        public async Task<ApiResponse> SetTodoPercent(int id)
+        public async Task<ApiResponse> SetTodoPercent(int id, int amount)
         {
-            var response = await _IToDoService.SetTodoPercent(id);
+            var result = ValidatorHelper.ValidateRequest(amount, new SetTodoPercentValidator());
+            if (result != null)
+            {
+                return result;
+            }
+            var response = await _IToDoService.SetTodoPercent(id, amount);
             return response;
         }
 
