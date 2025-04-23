b3fb4b7f573add9979718de1c8c428693aa2cb4e
Michal Tulej <michaltulej1@gmail.com>
2025-04-23 09:40:42 +0200

    Fluent Validators added

---
+++ ToDoApplication.Api/Validators/GetIncomingTodosValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ToDoApplication.Api.Validators
+{
+    public class GetIncomingTodosValidator : AbstractValidator<DateTime>
+    {
+        public GetIncomingTodosValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("Date is required.")
+                .WithErrorCode("DATE_REQUIRED")
+                .GreaterThanOrEqualTo(DateTime.Today)
+                .WithMessage("Date must be today or in the future.")
+                .WithErrorCode("DATE_PAST");
+        }
+    }
+}
---
+++ ToDoApplication.Api/Validators/SetTodoPercentValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ToDoApplication.Common.Models.Domain.Request;
+
+namespace ToDoApplication.Api.Validators
+{
+    public class SetTodoPercentValidator : AbstractValidator<int>
+    {
+        public SetTodoPercentValidator()
+        {
+            RuleFor(x => x)
+                .NotNull()
+                .WithMessage("Complete percent value is required.")
+                .WithErrorCode("PERCENT_REQUIRED")
+                .InclusiveBetween(0, 100)
+                .WithMessage("Complete percent must be between 0 and 100.")
+                .WithErrorCode("PERCENT_OUT_OF_RANGE");
+        }
+    }
+}
